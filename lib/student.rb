a5ec202f57b8c6f2bc2674a3e3d809c4d4f3adfe
Daniel Jenkala <djenkala@yahoo.com>
2020-02-06 22:48:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,7 +84,8 @@ class Student
     SELECT *
     FROM students
     WHERE grade = 10
-    LIMIT X
+    ORDER BY students.id
+    LIMIT ?
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)

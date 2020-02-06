e7f6e426c48a1bada466617740196fc754ede4c5
Daniel Jenkala <djenkala@yahoo.com>
2020-02-06 22:46:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,14 +79,14 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(X)
+  def self.first_X_students_in_grade_10
     sql = <<-SQL
     SELECT COUNT(*)
     FROM students
     WHERE grade = 12
     LIMIT X
     SQL
-    DB[:conn].execute(sql, X).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
